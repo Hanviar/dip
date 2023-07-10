de37e79d159678ff3e962ae88793b7fcd89a01d8
Hanviar <137649831+Hanviar@users.noreply.github.com>
2023-07-10 16:26:02 +0300

    Update core.py

--- core.py
+++ core.py
@@ -11,7 +11,7 @@ class VkTools:
     self.vkapi = vk_api.VkApi(token=access_token)
 
   def _bdate_toyear(self, bdate):
-      user_year = bdate.split('.')[2] if 'bdate' else none 
+      user_year = bdate.split('.') 
       now = datetime.now().year
       return now - int(user_year)
     
@@ -32,6 +32,19 @@ class VkTools:
                }
       
       return result
+
+    def get_city(self, city_name:
+        try:
+            cities = self.vkapi.method('database.getCities',
+                                       {
+                                         'q': city_name,
+                                         'count': 1
+                                       }
+                                       )
+            if len(cities['items']) > 0:
+                return cities['items'][0]
+        except ApiError as e:
+            print(f'error = {e}')
 
     def search_worksheet(self, params, offset):
       try:
@@ -56,26 +69,32 @@ class VkTools:
       
       return result
 
-    def get_photos(self, id):
-      try:
-          photos = self.vkapi.method('photos.get',
-                           {'owner_id': id,
-                           'album_id': 'profile',
-                           'extended': 1}
-                          )
-      except ApiError as e:
-          photos = {}
-          print(f'error = {e}')
+     def get_photos(self, id):
+        photos = self.api.method('photos.get',
+                                 {'user_id': id,
+                                  'album_id': 'profile',
+                                  'extended': 1
+                                  }
+                                 )
+        try:
+            photos = photos['items']
+        except KeyError:
+            return []
 
-      result = [{'owner_id': item['owner_id'],
-                  'id': item['id'],
-                 'likes': item['likes']['count'],
-                 'comments': item['comments']['count']
-      } for item in photos['items']
-      ]
-      !!!!!!
-      return result[:3]
+        res = []
 
+        for photo in photos:
+            res.append({'owner_id': photo['owner_id'],
+                        'id': photo['id'],
+                        'likes': photo['likes']['count'],
+                        'comments': photo['comments']['count'],
+                        }
+                       )
+
+        res.sort(key=lambda x: x['likes'] + x['comments'] * 10, reverse=True)
+
+        return res
+       
 if __name__ = '__main__':
   user_id =
   tools = VkTools(access_token)
