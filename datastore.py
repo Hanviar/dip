6726936a1a4cdc414c89b596ae1710df5158acb2
Hanviar <137649831+Hanviar@users.noreply.github.com>
2023-07-10 12:51:37 +0300

    Update datastore.py

--- datastore.py
+++ datastore.py
@@ -11,15 +11,24 @@ class Viewed(Base):
     profile_id = sq.Column(sq.Integer, primary_key=True)
     worksheet_id = sq.Column(sq.Integer, primary_key=True)
 
-engine = create_engine(db_url_object)
-Base.metadata.create_all(engine)
-with Session(engine) as session:
-    to_bd = Viewed(profile_id=1, worksheet_id=1)
-    session.add(to_bd)
-    session.commit()
 
-engine = create_engine(db_url_object)
-with Session(engine) as session:
-    from_bd = session.query(Viewed).filter(Viewed.profile_id==1).all()
-    for item in from_bd:
-        print(item.worksheet_id)
+
+def add_user(engine, profile_id, worksheet_id):
+    with Session(engine) as session:
+        to_bd = Viewed(profile_id=profile_id, worksheet_id=worksheet_id)
+        session.add(to_bd)
+        session.commit()
+
+def check_user(engine, profile_id, worksheet_id):
+    with Session(engine) as session:
+        from_bd = session.query(Viewed).filter(
+            Viewed.profile_id == profile_id,
+            Viewed.worksheet_id == worksheet_id).first()
+        return true if from_bd else false
+
+if '__name__' == '__main__':
+    engine = create_engine(db_url_object)
+    Base.metadata.create_all(engine)
+    add_user(engine, , )
+    res = check_user(engine, , )
+    print (res)
